f0221ab9461958664397073dd9d750a6b8b2849d
Soniah Keza <s.keza1@alustudent.com>
2025-06-19 02:09:52 -0700

    step cinqo

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -13,5 +13,23 @@ fi
 
 dir="submission_reminder_$recentName"
 submissions="$dir/assets/submissions.txt"
+
+if [ ! -d "$dir" ]; then
+	echo "Directory '$dir' not found, run create_environment.sh."
+	exit 1
+fi
+
 read -p "Enter the assignment to check: " task
 read -p "Enter the number of days remaining: " days
+task=$(echo "$task" | sed "s/$(echo -e '\u00a0')/ /g" | tr -cd '[:alnum:] [:space:]' | xargs)
+
+days=$(echo "$days" | xargs)
+
+echo "DEBUG: [$task]"
+
+if [ -z "$task" ] || ! [[ "$days" =~ ^[0-9]+$ ]]; then
+    echo "The Assignment name cannot be empty"
+    echo "and"
+    echo "The Days have to be numbers."
+    exit 1
+fi
