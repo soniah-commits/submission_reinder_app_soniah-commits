6a5f9efd6af90e6d70affbe84618467d03309d52
Soniah Keza <s.keza1@alustudent.com>
2025-06-19 01:53:42 -0700

    step quatro

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 read -p "Enter the same name as entered in the directory name: " recentName
+
+if [ -z "$recentName" ]; then
+	echo "Enter your recent directory name."
+	exit 1
+fi
+if ! [[ "$recentName" =~ ^[a-zA-Z\s]+$ ]]; then
+    echo "The Assignment name must contain."
+    echo "only letters and spaces."
+    exit 1
+fi
+
 dir="submission_reminder_$recentName"
 submissions="$dir/assets/submissions.txt"
 read -p "Enter the assignment to check: " task
